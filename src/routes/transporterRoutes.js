d5d005d4f305764182874119566e4f8417c79130
DipeshThakran <150577095+DipeshThakran@users.noreply.github.com>
2025-12-20 21:56:13 +0530

    Add transporter auth changes

--- src/routes/transporterRoutes.js
+++ src/routes/transporterRoutes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
-const { sendOtp } = require('../controllers/transporterController.js');
+const { sendOtp, verifyOtp, registerTransporter, loginTransporter } = require('../controllers/transporterController.js');
 
 router.post('/send-otp', sendOtp);
+router.post('/verify-otp', verifyOtp);
+router.post('/register', registerTransporter);
+router.post('/login', loginTransporter);
 
 module.exports = router;
